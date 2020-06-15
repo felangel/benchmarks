a7b025379da90461e8ee49f1f84e655e4504c694
Felix Angelov <felangelov@gmail.com>
2020-06-15 01:04:37 -0500

    additional updates

--- state_managers/lib/_bloc_lib/app.dart
+++ state_managers/lib/_bloc_lib/app.dart
@@ -8,49 +8,24 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'BLoC Lib Sample',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Page(title: 'BLoC Lib Sample'),
+      home: BlocProvider(
+        create: (_) => ItemsBloc(),
+        child: const Page(title: 'BLoC Lib Sample'),
+      ),
     );
   }
 }
 
-class Page extends StatefulWidget {
+class Page extends StatelessWidget {
   const Page({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _PageState createState() => _PageState();
-}
-
-class _PageState extends State<Page> {
-  final _itemsBloc = ItemsBloc();
-
-  @override
-  void initState() {
-    super.initState();
-    fill();
-  }
-
-  @override
-  void dispose() {
-    _itemsBloc.close();
-    super.dispose();
-  }
-
-  fill() async {
-    for (int i = 0; i < 10; i++) {
-      await Future.delayed(Duration(milliseconds: 500));
-      _itemsBloc.add(AddItemEvent(Item(title: DateTime.now().toString())));
-    }
-    print("It's done. Print now!");
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(title: Text(title)),
       body: BlocBuilder<ItemsBloc, List<Item>>(
-        bloc: _itemsBloc,
         builder: (context, items) {
           return ListView.builder(
             padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
